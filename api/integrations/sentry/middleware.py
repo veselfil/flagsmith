b5e431ca53d4a55df8bd9f1689ca472cbe806690
Matthew Elwell <matthewe@solidstategroup.com>
2022-02-03 15:43:01 +0000

    Tidy up sentry trace logic

--- api/integrations/sentry/middleware.py
+++ api/integrations/sentry/middleware.py
@@ -16,17 +16,18 @@ class ForceSentryTraceMiddleware:
     is set to the same value as the FORCE_SENTRY_TRACE_AUTH_KEY environment variable.
     """
 
-    FORCE_SENTRY_TRACE_HEADER = "X-Force-Sentry-Trace-Key"
+    FORCE_SENTRY_TRACE_HEADER = "HTTP_X_FORCE_SENTRY_TRACE_KEY"
 
     def __init__(self, get_response):
         self.get_response = get_response
         self.auth_key = settings.FORCE_SENTRY_TRACE_KEY
 
     def __call__(self, request):
-        auth = request.headers.get(self.FORCE_SENTRY_TRACE_HEADER)
+        auth = request.META.get(self.FORCE_SENTRY_TRACE_HEADER)
         if auth == self.auth_key:
-            transaction_name = f"{request.method} {request.path}"
-            with sentry_sdk.start_transaction(name=transaction_name, sampled=True):
+            with sentry_sdk.start_transaction(
+                op="http.server", name=request.path, sampled=True
+            ):
                 response = self.get_response(request)
         else:
             response = self.get_response(request)
